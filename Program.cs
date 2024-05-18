50596769258f1d5f7e8992df5e2e4d788723a9b1
Justin Simon <justinsimon@Justins-MBP.lan>
2024-05-18 15:27:43 -0700

    added comments in the program file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//set up sql server for vs code mac
+
+//added sqlserver package  
+
+//next configure docker and azure data studio to this program
+
+//watch this vid to connect to azure data studiogit init
+
+//https://www.google.com/search?sca_esv=ebfe579d5b8c900a&q=sql+server+mac&tbm=vid&source=lnms&prmd=ivnsmbt&sa=X&ved=2ahUKEwjDl8vLnJiGAxV5D0QIHTJTCZ4Q0pQJegQIExAB&biw=1090&bih=791&dpr=1.8#fpstate=ive&vld=cid:ac0bea9a,vid:3BFxALltQaM,st:0 
