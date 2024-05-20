b5634e1b24e2197c0accd7a64e7450effa9a9b8e
Justin Simon <justinsimon@Justins-MacBook-Pro.local>
2024-05-20 03:00:34 -0700

    Added basic sqlserver config

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
 //set up sql server for vs code mac
 
 //added sqlserver package  
@@ -10,3 +8,32 @@ Console.WriteLine("Hello, World!");
 //watch this vid to connect to azure data studiogit init
 
 //https://www.google.com/search?sca_esv=ebfe579d5b8c900a&q=sql+server+mac&tbm=vid&source=lnms&prmd=ivnsmbt&sa=X&ved=2ahUKEwjDl8vLnJiGAxV5D0QIHTJTCZ4Q0pQJegQIExAB&biw=1090&bih=791&dpr=1.8#fpstate=ive&vld=cid:ac0bea9a,vid:3BFxALltQaM,st:0 
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+namespace FlashCardConsoleApp
+{
+
+
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                //REMEMBER TO GET CONN STRING FROM AZURE STUDIOS
+                string connectionString = "CONNECTION STRING INSERTED HERE LATER FROM AZURE STUDIOS";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                       
+                    
+                }
+                
+
+
+
+            }
+    }
+}
--- obj/Debug/net8.0/FlashCardConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FlashCardConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlashCardConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50596769258f1d5f7e8992df5e2e4d788723a9b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlashCardConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlashCardConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
